ec475e67b50af0aa004a704d3f53e7984593edb0
Martin Dudek <martin.dudek@gmail.com>
2024-06-23 18:02:51 +0200

    benchmark added

---
+++ benchmark.py
@@ -0,0 +1,36 @@
+from efficient_kan_jax import KAN as EfficientKAN
+from fastkan_jax import  FastKAN
+from trainer import Trainer
+from mnist_data_loader import load_mnist_data
+import numpy as np
+
+def benchmark(model_cls, model_name, epochs):
+    print(f"Running benchmark for {model_name}...")
+
+    trainer = Trainer(model_cls=model_cls, layers_hidden=[28 * 28, 64, 10], data_loader=load_mnist_data,epochs=epochs)
+    epoch_times, train_losses, val_losses, val_accuracies = trainer.train_and_evaluate()
+    return {
+        "model_name": model_name,
+        "epoch_times": epoch_times,
+        "train_losses": train_losses,
+        "val_losses": val_losses,
+        "val_accuracies": val_accuracies,
+    }
+
+if __name__ == "__main__":
+    results = []
+
+    epochs = 10
+
+    # Benchmark EfficientKAN
+    results.append(benchmark(EfficientKAN, "EfficientKAN",epochs=epochs))
+
+    # Benchmark FastKAN
+    results.append(benchmark(FastKAN, "FastKAN",epochs=epochs))
+
+    for result in results:
+        print(f"Benchmarking {result['model_name']}")
+        print(f"Average Epoch Time: {np.mean(result['epoch_times']):.2f}s")
+        print(f"Final Training Loss: {result['train_losses'][-1]:.4f}")
+        print(f"Final Validation Loss: {result['val_losses'][-1]:.4f}")
+        print(f"Final Validation Accuracy: {result['val_accuracies'][-1]:.4f}\n")
--- trainer.py
+++ trainer.py
@@ -3,6 +3,7 @@ import jax.numpy as jnp
 import optax
 from flax.training import train_state
 from tqdm import tqdm
+import time
 
 def cross_entropy_loss(logits, labels):
     return optax.softmax_cross_entropy_with_integer_labels(logits, labels).mean()
@@ -46,12 +47,19 @@ class Trainer:
             self.trainloader, self.valloader = None, None
 
     def train_and_evaluate(self):
+        epoch_times = []
+        train_losses = []
+        val_losses = []
+        val_accuracies = []
+
         for epoch in range(self.epochs):
+            start_time = time.time()
+
             train_loss = 0
             train_accuracy = 0
             update_frequency = 10
 
-            with tqdm(self.trainloader, desc=f"Epoch {epoch + 1} [Training]") as pbar:
+            with tqdm(self.trainloader, desc=f"Epoch {epoch + 1}/{self.epochs} [Training]") as pbar:
                 cumulative_loss = 0.0
                 cumulative_accuracy = 0.0
                 num_batches = len(self.trainloader)
@@ -71,18 +79,19 @@ class Trainer:
                         avg_loss = cumulative_loss / update_frequency
                         avg_accuracy = cumulative_accuracy / update_frequency
 
-                        pbar.set_postfix(loss=avg_loss, accuracy=avg_accuracy, lr=self.learning_rate)
+                        pbar.set_postfix(loss=avg_loss, accuracy=avg_accuracy)
 
                         cumulative_loss = 0.0
                         cumulative_accuracy = 0.0
 
             train_loss /= len(self.trainloader)
             train_accuracy /= len(self.trainloader)
-            print(f"Epoch {epoch + 1} [Training]: Loss: {train_loss}, Accuracy: {train_accuracy}\n")
+            train_losses.append(train_loss)
+            print(f"Epoch {epoch + 1}/{self.epochs} [Training]: Loss: {train_loss}, Accuracy: {train_accuracy}\n")
 
             val_loss = 0
             val_accuracy = 0
-            with tqdm(self.valloader, desc=f"Epoch {epoch + 1} [Validation]") as pbar:
+            with tqdm(self.valloader, desc=f"Epoch {epoch + 1}/{self.epochs}  [Validation]") as pbar:
                 cumulative_loss = 0.0
                 cumulative_accuracy = 0.0
                 num_batches = len(self.valloader)
@@ -108,9 +117,16 @@ class Trainer:
 
             val_loss /= len(self.valloader)
             val_accuracy /= len(self.valloader)
-            print(f"Epoch {epoch + 1} [Validation]: Loss: {val_loss}, Val Accuracy: {val_accuracy}\n")
+            val_losses.append(val_loss)
+            val_accuracies.append(val_accuracy)
+            print(f"Epoch {epoch + 1}/{self.epochs}  [Validation]: Loss: {val_loss}, Val Accuracy: {val_accuracy}\n")
+
+            end_time = time.time()
+            epoch_times.append(end_time - start_time)
 
             self.learning_rate *= 0.8
             self.tx = optax.adamw(learning_rate=self.learning_rate, weight_decay=self.weight_decay)
             self.state = train_state.TrainState.create(
                 apply_fn=self.model.apply, params=self.state.params, tx=self.tx)
+
+        return epoch_times, train_losses, val_losses, val_accuracies
